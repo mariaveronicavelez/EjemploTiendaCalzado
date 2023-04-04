07736438db1b792f3de305dc7beff6587cd5c421
veronica.velez <mavevelez9@gmail.com>
2023-04-03 23:39:02 -0300

    Se crea proyecto y se agregan estructuras de package's. Se crean clases del modelo y clases en package Main para ejemplificar conceptos

---
+++ src/modelo/Producto.java
@@ -0,0 +1,17 @@
+package modelo;
+
+public class Producto {
+
+    //Atributos
+    /*
+    codigo    (String) "123ABC"
+    stock      int     100
+    valorBase  int    10000*/
+    public String codigo;
+    public int stock;
+    public int valorBase;
+
+
+    //Codigo constructor por defecto
+
+}
